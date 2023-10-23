90bc77d29a49b7c0099b35ee2df858f0b7ada8ca
confused-Techie <dev@lhbasics.com>
2023-10-22 21:27:58 -0700

    Convert to JS and remove all typescript artifacts

--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export default function translateLines(lines: any, repositoryPath: any, fileName: any, commitSha: any): Promise<Map<any, any>>;
-export declare function getDiff(respositoryPath: any, fileName: any, commitSha: any): Promise<string>;
-export declare function translateLinesGivenDiff(lines: number[], diffInput: any): Map<any, any>;
-export declare function diffPositionToFilePosition(positions: number[], diffInput: any): Map<any, any>;
